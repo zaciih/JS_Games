61518e364d24b0be544c786f32dbc29e180d4243
zaciih <36475659+zaciih@users.noreply.github.com>
2019-07-27 20:35:48 +0100

    player position added to player class

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,6 @@
 $(function(){
   class Player {
-    constructor(player, top, bottom, left, right, height, width) {
+    constructor() {
       this.player = $("#player");
       this.height = this.player.height();
       this.width = this.player.width();
@@ -8,18 +8,23 @@ $(function(){
       this.bottom = this.top + this.height;
       this.left = this.player.offset().left;
       this.right = this.left + this.width;
-      }
-    }
+      this.posx = 5;
+      this.posy = 5;
+      };
+    update_player(top, bottom, left, right){
+      top = this.player.offset().top;
+      bottom = this.top + this.height;
+      left = this.player.offset().left;
+      right = this.left + this.width;
+      move_player(top, bottom, left, right);
+      console.log("player posx = "+ player.posx);
+      console.log("wall = "+ background_right);
+      };
+    };
 
 
   var player = new Player();
-  // var player = $("#player");
-  // console.log("player height = "+ player.height);
-  // console.log("player width = "+ player.width);
-  // console.log("player top = "+ player.top);
-  // console.log("player bottom = "+ player.bottom);
-  // console.log("player left = "+ player.left);
-  // console.log("player right = "+ player.right);
+
   var container = $("#game_area");
   var container_top = container.offset().top;
   var container_bottom = container_top + container.height();
@@ -29,22 +34,10 @@ $(function(){
 
   // game_start();
 
-  const player_pos = {
-    x: container.width()/2 - player.width/2,
-    y: container.height()-100 - player.height/2,
-    };
-
-    // console.log("player_pos = "+ player_pos);
-
   var background = $("#background");
-
-  // var player_left = player.offset().left
-  // var player_right = player_left + player.width();
-  // var player_top = player.offset().top;
-  // var player_bottom = player_top + player.height();
   var background_right = container_right - player.width;
-  var left = false;
-  var right = false;
+  var player_left = false;
+  var player_right = false;
   var jump = false;
 
   var gravity = 0.1;
@@ -59,12 +52,12 @@ $(function(){
     switch (e.which) {
         case 37:
         case 65:
-          left = true;
+          player_left = true;
         break;
 
         case 39:
         case 68:
-          right = true;
+          player_right = true;
         break;
 
         case 38:
@@ -81,12 +74,12 @@ $(function(){
     switch (e.which) {
       case 37:
       case 65:
-        left = false;
+        player_left = false;
       break;
 
       case 39:
       case 68:
-        right = false;
+        player_right = false;
       break;
 
       case 38:
@@ -107,42 +100,33 @@ $(function(){
       container_bottom = container_top + container.height();
       container_left = container.offset().left;
       container_right = container_left + container.width();
-      background_right = container_right - player.width*2;
-      player_left = player.left
-      player_right = player.left + player.width;
-      player_top = player.top;
-      player_bottom = player.top + player.height;
+      background_right = container_right - (player.width*2);
+      player.update_player();
       player_floor = container_bottom - (player.height*2);
-      move_player(player_bottom, container_bottom, background_right, container_left,
-         player_floor, player);
-      console.log("player posy = "+ player_pos.y);
-      console.log("floor = "+ player_floor);
-
       move_background(background_right, player_right);
       }, 10);
     // };
 
-  function move_player(player_bottom, container_bottom, background_right,
-     container_left, player_floor, player){
+  function move_player(top, bottom, left, right){
     player.player.css({
-        'left': player_pos.x + "px",
-        'top': player_pos.y + "px"
+        'left': player.posx + "px",
+        'top': player.posy + "px"
       });
-    player_pos.y += gravityspeed;
+    player.posy += gravityspeed;
     gravityspeed += gravity;
-    if (player_left >= container_left && left == true) {
-      player_pos.x -=2;
+    if (left >= container_left && player_left == true) {
+      player.posx -=2;
       }
-    if (player_right <= background_right && right == true) {
-        player_pos.x +=2;
+    if (right <= background_right && player_right == true) {
+        player.posx +=2;
       };
-    if (player_pos.y >= player_floor) {
+    if (player.posy >= player_floor) {
       // console.log(true);
-      player_pos.y = player_floor;
+      player.posy = player_floor;
       gravityspeed = 0;
       };
-    if (player_pos.y >= player_floor && jump == true) {
-        player_pos.y -= 10;
+    if (player.posy >= player_floor && jump == true) {
+        player.posy -= 10;
         gravityspeed = grav_decrease;
       };
     };
